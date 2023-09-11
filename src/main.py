6c92143bda69df2bbd6db733b6d443b295fe58a0
Ganesh Tiwari <ganesht049@gmail.com>
2023-09-11 22:12:58 +0530

    feat: changed exposed port definition

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from typing import Union
 import json
+import uvicorn
+import os
 
 from fastapi import FastAPI, Request
 
 app = FastAPI()
-
+port = os.getenv('PORT', 3000)
+if type(port) == str:
+  port = int(port)
 
 @app.get("/")
 def read_root(request: Request):
@@ -15,3 +19,6 @@ def read_root(request: Request):
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
   return {"item_id": item_id, "q": q}
+
+if __name__ == '__main__':
+  uvicorn.run(app, host="0.0.0.0", port=port)
