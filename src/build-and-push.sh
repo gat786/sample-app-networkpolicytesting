390397743dcf3b70719a0427dbf96d66d4db18aa
Ganesh Tiwari <ganesht049@gmail.com>
2023-09-11 21:24:20 +0530

    fix: dockerfile and imagepullplicy

--- src/build-and-push.sh
+++ src/build-and-push.sh
@@ -1,5 +1,5 @@
-docker build . --tag fastapi-np-test-app
+docker build . --tag fastapi
 
-docker tag fastapi-np-test-app us-east1-docker.pkg.dev/bhdigital-data-dev/asset-transfer-artifact-registry-dev/fastapi-np-test
+docker tag fastapi us-east1-docker.pkg.dev/bhdigital-data-dev/asset-transfer-artifact-registry-dev/fastapi-np-test
 
 docker push us-east1-docker.pkg.dev/bhdigital-data-dev/asset-transfer-artifact-registry-dev/fastapi-np-test
